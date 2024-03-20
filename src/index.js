afebd98f751a40f6a2575781560d842a802a35a8
Bluce <a1019521a@gmail.com>
2024-03-20 15:09:23 +0800

    vercel.json

--- src/index.js
+++ src/index.js
@@ -37,19 +37,19 @@ fastify.register(salesDataRoute);
 
 
 // 啟動 Fastify 伺服器
-const start = async () => {
-  try {
-    await fastify.listen({ port: 3000 });
+// const start = async () => {
+//   try {
+//     await fastify.listen({ port: 3000 });
 
-    const address = fastify.server.address();
-    const port = typeof address === "string" ? address : address?.port;
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-};
+//     const address = fastify.server.address();
+//     const port = typeof address === "string" ? address : address?.port;
+//   } catch (err) {
+//     fastify.log.error(err);
+//     process.exit(1);
+//   }
+// };
 
-start();
+// start();
 
 
 module.exports = async (req, res) => {
