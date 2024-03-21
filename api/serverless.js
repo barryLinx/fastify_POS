c4c3ea9799962f53111e41067f72e6507ba22abf
Bluce <a1019521a@gmail.com>
2024-03-21 11:26:07 +0800

    Vercel.json

--- api/serverless.js
+++ api/serverless.js
@@ -1,45 +1,4 @@
 "use strict";
-
-// const fs = require('fs');
-// const fastify = require("fastify")({ 
-//   logger: true ,
-//   // https: {
-//   //   key: fs.readFileSync('E:/fastify-pos/localhost.key'),
-//   //   cert: fs.readFileSync('E:/fastify-pos/localhost.crt')
-//   // }
-// });
-// require("dotenv").config();
-// //const verifyJWT = require('./middleware/verifyJWT');
-
-// const authentication=require('../src/plugins/authentication');
-// const authRoutes = require("../src/routes/authRoute");
-// const menuDataRoute = require("../src/routes/menuDataRoute");
-// const salesDataRoute = require("../src/routes/salesRoute");
-
-// // 註冊插件 fastify-jwt , 解析 accessToken
-// fastify.register(require("@fastify/jwt"), {
-//   secret: process.env.ACCESS_TOKEN_SECRET,
-// });
-
-//  fastify.register(require('@fastify/cors'), {
-//   origin:"*",
-//   credentials:true,
-  
-// })
-
-// fastify.register(require("@fastify/cookie"));
-// //註冊 authentication 插件，例如fastify-jwt
-
-// fastify.register(authentication);
-// // 註冊路由
-// fastify.register(authRoutes);
-// fastify.register(menuDataRoute);
-// fastify.register(salesDataRoute);
-
-// fastify.get('/',async (request, reply) => {
-//   reply.send({ hello: 'world',message:"deploy success" });
-// });
-
 const fastify = require("../src/index");
 
 module.exports = async (req, res) => {
