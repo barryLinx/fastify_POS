e95d04423f52849fb62a005639c56ba26153c723
Bluce <a1019521a@gmail.com>
2024-03-21 11:04:55 +0800

    add serverless

--- api/serverless.js
+++ api/serverless.js
@@ -1,25 +1,53 @@
 "use strict";
 
-
-const Fastify = require("fastify")({ 
+const fs = require('fs');
+const fastify = require("fastify")({ 
   logger: true ,
- 
+  // https: {
+  //   key: fs.readFileSync('E:/fastify-pos/localhost.key'),
+  //   cert: fs.readFileSync('E:/fastify-pos/localhost.crt')
+  // }
+});
+require("dotenv").config();
+//const verifyJWT = require('./middleware/verifyJWT');
+
+const authentication=require('../src/plugins/authentication');
+const authRoutes = require("../src/routes/authRoute");
+const menuDataRoute = require("../src/routes/menuDataRoute");
+const salesDataRoute = require("../src/routes/salesRoute");
+
+
+
+// 註冊插件 fastify-jwt , 解析 accessToken
+fastify.register(require("@fastify/jwt"), {
+  secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-// Instantiate Fastify with some config
-const app = Fastify({
-  logger: true,
-});
+ fastify.register(require('@fastify/cors'), {
+  origin:"http://localhost:8081",
+  credentials:true,
+  
+})
 
-// Register your application as a normal plugin.
-app.register(import("../src/index"));
+fastify.register(require("@fastify/cookie"));
+//註冊 authentication 插件，例如fastify-jwt
+
+fastify.register(authentication);
+// 註冊路由
+fastify.register(authRoutes);
+fastify.register(menuDataRoute);
+fastify.register(salesDataRoute);
+
+fastify.get('/test',async (request, reply) => {
+  reply.send({ hello: 'world',message:"deploy success" });
+});
 
 module.exports = async (req, res) => {
   try {
     // 等待 Fastify 啟動成功，才能將 HTTP 請求轉發給 Fastify 伺服器
-    await app.ready();
+    await fastify.ready();
   //
-  app.server.emit('request', req, res);
+  fastify.server.emit('request', req, res);
   } catch (error) {
     // 如果出现错误，返回错误信息
     res.status(500).send('Internal Server Error');
