6b1625157a4f1956a749a105ea5cbeaaa000e2d3
Bluce <a1019521a@gmail.com>
2024-03-21 11:07:43 +0800

    add serverless

--- api/serverless.js
+++ api/serverless.js
@@ -38,7 +38,7 @@ fastify.register(authRoutes);
 fastify.register(menuDataRoute);
 fastify.register(salesDataRoute);
 
-fastify.get('/test',async (request, reply) => {
+fastify.get('/',async (request, reply) => {
   reply.send({ hello: 'world',message:"deploy success" });
 });
 
